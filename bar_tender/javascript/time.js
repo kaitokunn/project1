2466ad6322a24725010aa87f72ee1e0882d997e3
kaito <kaito.endenulu@gmail.com>
2019-01-12 16:25:51 +0900

    更新

--- bar_tender/javascript/time.js
+++ bar_tender/javascript/time.js
@@ -1,6 +1,6 @@
 var i = 0;
 //var url = "https://web.sfc.keio.ac.jp/~t18381ks/bar_tender/images/" //背景画像保存先
-var url = "file:///Users/kaito/Desktop/images/" //背景画像保存先
+var url = "../images/" //背景画像保存先
 //配列を作成
 
 var img = new Array("num1.jpg","num2.jpg","num3.jpg","num4.jpg","num5.jpg","num6.jpg","num7.jpg","num8.jpg");
